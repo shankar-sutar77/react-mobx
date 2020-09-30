6d901f6deff2d906c6a1cfccba993a220f02caac
shankar <shankarsutar77@gmail.com>
2020-09-30 11:03:10 +0530

    initial commit

---
+++ react-mobx-demo/src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default class HomePage extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Hello from home page</h1>
+            </div>
+        )
+    }
+}
